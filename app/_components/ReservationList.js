eabd9ace5bdef34b228c7723876dbe937c0fa9ea
root12 <abdullahalmasrur8@gmail.com>
2024-10-12 03:12:42 +0600

    init

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,34 @@
+"use client";
+
+import { useOptimistic } from "react";
+import { deleteBooking } from "../_lib/action";
+import ReservationCard from "./ReservationCard";
+import DeleteReservation from "./DeleteReservation";
+
+function ReservationList({ bookings }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    (curBookings, bookingId) => {
+      return curBookings.filter((booking) => booking.id !== bookingId);
+    }
+  );
+
+  async function handleDelete(bookingId) {
+    optimisticDelete(bookingId);
+    await DeleteBooking(bookingId);
+  }
+
+  return (
+    <ul className="space-y-6">
+      {optimisticBookings.map((booking) => (
+        <ReservationCard
+          booking={booking}
+          key={booking.id}
+          onDelete={handleDelete}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
---
+++ app/layout.js
@@ -0,0 +1,36 @@
+import Header from "./_components/Header";
+import Logo from "./_components/Logo";
+import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+import { ReservationProvider } from "./_components/ReservationContext";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: {
+    template: "%s / Hotel Management",
+    default: "Welcome / Hotel Management",
+  },
+  description:
+    "Luxurious Cabin Hotel, located in the heart of the Italian surrounded by beautiful mountains and dark forests",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
+        </div>
+      </body>
+    </html>
+  );
+}
